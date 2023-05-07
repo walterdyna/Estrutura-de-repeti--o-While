36d9d8b39e24f54f0de7d8213345c2b275f76b3d
walterdyna <alexdyna@gmail.com>
2023-05-07 19:02:19 -0300

    Estrutura de repetição WHILE

---
+++ README.md
@@ -0,0 +1,9 @@
+# Estrutura de repetição WHILE - JavaScript
+
+----------------------------------- Estrutura de repetição WHILE -----------------------------------
+
+While ----> É usada para repetir um bloco de código enquanto uma condição especificada for verdadeira. 
+A condição é verificada antes de cada execução do bloco de código, e se a condição for falsa desde o 
+início, o bloco de código nunca será executado. É uma forma de criar loops.
+
+*/
---
+++ While.js
@@ -0,0 +1,22 @@
+/*
+
+----------------------------------- Estrutura de repetição WHILE -----------------------------------
+
+While ----> É usada para repetir um bloco de código enquanto uma condição especificada for verdadeira. 
+A condição é verificada antes de cada execução do bloco de código, e se a condição for falsa desde o 
+início, o bloco de código nunca será executado. É uma forma de criar loops.
+
+*/
+
+while (true){
+    console.log('Vai ficar imprimindo ate ser Falso') // Vai ficar imprimendo ate ser Falso...
+}
+
+let i = 0
+
+while (i < 20){
+    i+=2
+    console.log(i)
+    console.log('Vai imprimir contando mais 2')
+}
+
